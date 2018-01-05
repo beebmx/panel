f15b060ae6b35c78ff8e24072267e2f55e9a2d0a
Mary Soco Ortiz <marysoco.ortiz@gmail.com>
2018-01-05 16:31:52 -0600

    Added radio field

---
+++ src/resources/assets/js/ui/fields/RadioField.vue
@@ -0,0 +1,38 @@
+<template>
+    <panel-field v-bind="$props">
+        <label class="label" :for="id" v-text="label"></label>
+            <div class="control">
+                <label class="radio" v-for="option in options" :key="option">
+                    <panel-radio-input v-bind="$props" :value="option" v-model="data"/> {{option}} 
+                </label>
+            </div>
+        <p v-if="help" class="help" v-text="help"></p>
+    </panel-field>
+</template>
+
+<script>
+import field from '../props/field'
+import input from '../props/input'
+
+export default {
+    mixins: [field, input],
+    props: {
+        options: {
+            type: [Object]
+        },
+    },
+    data () {
+        return {
+            data: this.value
+        }
+    },
+    watch: {
+        value(value) {
+            this.data = value
+        },
+        data() {
+            this.$emit('input', {id:this.id, value:this.data })
+        }
+    }
+}
+</script>
---
+++ src/resources/assets/js/ui/inputs/RadioInput.vue
@@ -0,0 +1,34 @@
+<template>
+        <input class="radio"
+            :class="[size, design]"
+            type="radio"
+            :name="id"
+            :value="value"
+            :required="required"
+            :readonly="readonly"
+            :disabled="disabled"
+            :checked="ischecked"
+            @change="update"/>
+</template>
+
+<script>
+import props from '../props/input'
+
+export default {
+    mixins: [props],
+    model: {
+        prop: 'data',
+        event: 'change'
+    },
+    computed:{
+        ischecked(){
+            return this.$parent.$parent.data == this.value
+        }    
+    },
+    methods: {
+        update(event) {
+            this.$emit('change', this.value )
+        }
+    }
+}
+</script>
