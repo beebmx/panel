1a622cca8e163965210e0415d3254443c5c0f8c7
Mary Soco Ortiz <marysoco.ortiz@gmail.com>
2018-01-15 16:52:23 -0600

    Added align to radio

--- src/resources/assets/js/ui/fields/RadioField.vue
+++ src/resources/assets/js/ui/fields/RadioField.vue
@@ -1,10 +1,12 @@
 <template>
     <panel-field v-bind="$props">
         <label class="label" :for="id" v-text="label"></label>
-            <div class="field" v-for="option in options" :key="option">
+        <div class="control" :class="haveColumns('columns')">
+            <div class="field" :class="haveColumns('column')" v-for="option in options" :key="option">
                 <panel-radio-input v-bind="$props" :value="option" v-model="data"/> 
                 <label :for="id+'-'+option" v-text="option"></label>
             </div>
+        </div>
         <p v-if="help" class="help" v-text="help"></p>
     </panel-field>
 </template>
@@ -23,6 +25,20 @@ export default {
     data () {
         return {
             data: this.value
+        }
+    },
+    computed :{
+        isHorizontal(){
+            return this.align === 'horizontal'
+        }
+    },
+    methods :{
+        haveColumns (type){
+            if (type=="columns"){
+                return this.isHorizontal ? 'columns is-multiline' : ''
+            }else{
+                return this.isHorizontal ? 'column is-narrow' : ''
+            }
         }
     },
     watch: {
